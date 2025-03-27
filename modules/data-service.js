1421279274c9aa316d1eb2cf92a62b9b9c7b3f71
pkaur526@myseneca.ca <Prabhleen407>
2025-03-26 22:05:00 -0400

    Final commit and .env is ignored

--- modules/data-service.js
+++ modules/data-service.js
@@ -1,68 +1,174 @@
-const siteData = require("../data/NHSiteData");
-const provinceAndTerritoryData = require("../data/provinceAndTerritoryData");
 
-let sites = [];
 
+require("dotenv").config();
+const Sequelize = require("sequelize");
+require('pg'); 
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      port: 5432,
+      dialectOptions: {
+        ssl: { rejectUnauthorized: false },
+      },
+    }
+  );
+  
+  const ProvinceOrTerritory = sequelize.define(
+    "ProvinceOrTerritory",
+    {
+      code: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      name: Sequelize.STRING,
+      type: Sequelize.STRING,
+      region: Sequelize.STRING,
+      capital: Sequelize.STRING,
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+  
+  const Site = sequelize.define(
+    "Site",
+    {
+      siteId: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      site: Sequelize.STRING,
+      description: Sequelize.TEXT,
+      date: Sequelize.INTEGER,
+      dateType: Sequelize.STRING,
+      image: Sequelize.STRING,
+      location: Sequelize.STRING,
+      latitude: Sequelize.FLOAT,
+      longitude: Sequelize.FLOAT,
+      designated: Sequelize.INTEGER,
+      provinceOrTerritoryCode: Sequelize.STRING,
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+  
+  Site.belongsTo(ProvinceOrTerritory, {
+    foreignKey: "provinceOrTerritoryCode",
+  });
+  
 function initialize() {
   return new Promise((resolve, reject) => {
-    siteData?.forEach(site => {
-      let siteWithProvinceOrTerritory = { ...site, provinceOrTerritoryObj: provinceAndTerritoryData.find(provOrTerr => provOrTerr.code == site.provinceOrTerritoryCode) }
-      sites.push(siteWithProvinceOrTerritory);
-      resolve();
-    });
+    sequelize.sync().
+    then(()=>resolve())
+    .catch(err=>reject("No site retrieved"));
   });
 
 }
 
 function getAllSites() {
   return new Promise((resolve, reject) => {
-    resolve(sites);
+    Site.findAll({include: [ProvinceOrTerritory]})
+    .then(data=>resolve(data))
+    .catch(err=>reject(err.message))
   });
 }
 
 function getSiteById(id) {
 
   return new Promise((resolve, reject) => {
-    let foundSite = sites.find(s => s.siteId == id);
-
-    if (foundSite) {
-      resolve(foundSite)
-    } else {
-      reject("Unable to find requested site");
-    }
+   Site.findAll({
+    include: [ProvinceOrTerritory],
+    where:{siteId:id}
+    }).then(data=>{
+        if(data.length >0){
+            resolve(data[0]);
+        }else{
+            reject("Unable to find requested sites");
+        }
+    }).catch(()=>reject("Unable to find requested sites"));
   });
 }
 
 function getSitesByProvinceOrTerritoryName(name) {
-
-  return new Promise((resolve, reject) => {
-    let foundSites = sites.filter(s => s.provinceOrTerritoryObj.name.toUpperCase().includes(name.toUpperCase()));
-
-    if (foundSites.length > 0) {
-      resolve(foundSites)
-    } else {
-      reject("Unable to find requested sites");
+Site.findAll({
+    include: [ProvinceOrTerritory], where: { 
+        '$ProvinceOrTerritory.provinceOrTerritory$': {
+          [Sequelize.Op.iLike]: `%${provinceOrTerritory}%`
+        }
+      }
+       
+}).then(data=>{
+    if(data.length >0){
+        resolve(data);
+    }else{
+        reject("Unable to find requested sites");
     }
-  });
-
-}
+}).catch(()=>reject("Unable to find requested sites"));
+};
 
 function getSitesByRegion(region) {
-  console.log("region:");
+    return new Promise((resolve, reject) => {
+      Site.findAll({
+        include: [ProvinceOrTerritory],
+        where: {
+          "$ProvinceOrTerritory.region$": {
+            [Sequelize.Op.iLike]: `%${region}%`
+          }
+        }
+      }).then(data => {
+        if (data.length > 0) {
+          resolve(data);
+        } else {
+          reject("Unable to find requested sites");
+        }
+      }).catch(() => reject("Unable to find requested sites"));
+    });
+  }
+  
+  function addSite(siteData) {
+    return new Promise((resolve, reject) => {
+      Site.create(siteData)
+        .then(() => resolve())
+        .catch(err => reject(err.errors[0].message));
+    });
+  }
+  
+  function getAllProvincesAndTerritories() {
+    return new Promise((resolve, reject) => {
+      ProvinceOrTerritory.findAll()
+        .then(data => resolve(data))
+        .catch(() => reject("Unable to retrieve provinces/territories"));
+    });
+  }
+  
+  function editSite(id, siteData) {
+    return new Promise((resolve, reject) => {
+      Site.update(siteData, {
+        where: { siteId: id }
+      })
+        .then(() => resolve())
+        .catch(err => reject(err.errors[0].message));
+    });
+  }
 
-  return new Promise((resolve, reject) => {
-    let foundSites = sites.filter(s => s.provinceOrTerritoryObj.region.toUpperCase().includes(region.toUpperCase()));
-
-    if (foundSites.length > 0) {
-      resolve(foundSites)
-    } else {
-      reject("Unable to find requested sites");
-    }
-  });
-
-}
-
-
-module.exports = { initialize, getAllSites, getSiteById, getSitesByRegion, getSitesByProvinceOrTerritoryName }
+  function deleteSite(id) {
+    return new Promise((resolve, reject) => {
+      Site.destroy({ where: { siteId: id } })
+        .then(() => resolve())
+        .catch(err => reject(err.errors[0].message));
+    });
+  }
+  
+  
+module.exports = { initialize, getAllSites, getSiteById, getSitesByRegion,
+   getSitesByProvinceOrTerritoryName,addSite,getAllProvincesAndTerritories 
+  ,editSite,  deleteSite};
 
 
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 /********************************************************************************
-*  WEB322 – Assignment 04
+*  WEB322 – Assignment 05
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: _____________Prabhleen Kaur_________ Student ID: _151653227_____________ Date: ____2025-03-11__________
+*  Name: _____________Prabhleen Kaur_________ Student ID: _151653227_____________ Date: ____2025-03-26__________
 *
-*  Published URL:  
-*
+*  Published URL:  https://vercel.com/prabhleen-kaurs-projects-90f567b5/assignment-3-ccav/ARXT8wfzwAkpRaax8hb62K8sMyNn
+*IT'S GIVING ME INTERNAL SERVER ERROR.
 ********************************************************************************/
 
 
 
+require('dotenv').config(); 
 const express = require("express");
 const siteData = require("./modules/data-service");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + "/public"));
+express.urlencoded({extended:true});
 
 // Home Route
 app.get("/", (req, res) => {
@@ -66,7 +69,6 @@ app.get("/sites", async (req, res) => {
     }
 });
 
-// Single Site Route by ID
 app.get("/sites/:id", async (req, res) => {
     try {
         let site = await siteData.getSiteById(req.params.id);
@@ -78,6 +80,55 @@ app.get("/sites/:id", async (req, res) => {
         res.status(404).render("404", { message: err.message });
     }
 });
+
+app.get("/addSite",async (req,res) =>{
+    try{
+        const provincesAndTerritories =await siteData.getAllProvincesAndTerritories();
+        res.render("addSite", { provincesAndTerritories: provincesAndTerritories });
+  }catch (err) {
+    res.render("500", { message: `I'm sorry,but the following error occured: ${err}` });
+  }
+});
+
+app.post("/addSite",async(req,res) => {
+    try{
+        await siteData.addSite(req.body);
+        res.redirect("/sites");
+    }catch(err){
+        res.render("500", { message: `I'm sorry, but the following error occured: ${err}` });
+    }
+})
+
+app.get("/editSite/:id", async (req, res) => {
+    try {
+      const site = await siteData.getSiteById(req.params.id);
+      const provincesAndTerritories = await siteData.getAllProvincesAndTerritories();
+      res.render("editSite", { site, provincesAndTerritories });
+    } catch (err) {
+      res.status(404).render("404", { message: err });
+    }
+  });
+  
+
+  app.post("/editSite", async (req, res) => {
+    try {
+      await siteData.editSite(req.body.siteId, req.body);
+      res.redirect("/sites");
+    } catch (err) {
+      res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` ,
+      });
+    }
+  });
+
+  app.get("/deleteSite/:id", async (req, res) => {
+    try {
+      await siteData.deleteSite(req.params.id);
+      res.redirect("/sites");
+    } catch (err) {
+      res.render("500", 
+        {message: err.errors[0].message});
+    }
+  });
 
 // 404 Middleware for Undefined Routes
 app.use((req, res, next) => {
@@ -92,3 +143,4 @@ siteData.initialize().then(() => {
 }).catch(err => {
     console.error("Failed to initialize data-service:", err);
 });
+
